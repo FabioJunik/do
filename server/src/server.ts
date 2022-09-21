5cfca94f11804bcc3198f9af31b5c3a4d3b64428
FabioJunik <fabiojunik@gmail.com>
2022-09-21 12:36:38 +0100

    feat: dealing with erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,25 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+})
 
 app.listen(5000, () => console.log('Server is runnig in port 5000'))
 
