b071d7601c5fd5bb3ca4cb9b109481ee1064cfbc
FabioJunik <fabiojunik@gmail.com>
2022-09-24 16:28:06 +0100

    feat(front): creating unassigned task

--- web/src/components/CreateTaskModal.tsx
+++ web/src/components/CreateTaskModal.tsx
@@ -1,19 +1,43 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import axios from 'axios';
+import { FormEvent, useState } from 'react';
 import { Content, Overlay, Title, Close } from '../styles/modalStyles';
 
 
 
 export function CreateTaskModal() {
+    const [description, setDescription] = useState('')
 
+
+    async function handleCreateEmployee(event: FormEvent) {
+        event.preventDefault();
+
+        try {
+            await axios.post("http://localhost:5000/unassignedTasks", {
+                description
+            })
+
+            setDescription('');
+            alert("Tarefa adicionada com sucesso !");
+
+        } catch (err) {
+            alert("Erro ao adicionar");
+            console.log(err);
+        }
+
+
+    }
     return (
         <Dialog.Portal>
             <Overlay>
                 <Content>
                     <Title>Adicionar tarefa</Title>
-                    <form>
+                    <form onSubmit={handleCreateEmployee}>
                         <div>
                             <label htmlFor="description">Descrição*</label>
                             <input
+                                onChange={(e) => setDescription(e.target.value)}
+                                value={description}
                                 id='description'
                                 name='description'
                                 placeholder="Qual é tarefa"
